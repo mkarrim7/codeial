6868d88194708aa9d4f8488ca8e7321fa16f9cb0
manikanta <manikantareddy8247@gmail.com>
2022-12-09 09:35:20 +0530

    add header and footer

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>Working on controllers page</h1>');
+    return res.render('home',{
+        title:"Home"
+    });
 }
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>You are in User Profile Page</h1>');
+    return res.render('user',{
+        title:"User"
+    })
 }
